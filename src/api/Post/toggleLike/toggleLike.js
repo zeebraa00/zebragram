40f627bbbdc3704e49608e3a14b27b278814c855
zeebraa00 <zeebraa00@gmail.com>
2020-08-25 05:21:23 +0900

    [add] 게시물 좋아요

---
+++ src/api/Post/toggleLike/toggleLike.js
@@ -0,0 +1,48 @@
+import { isAuthenticated } from "../../../middlewares"
+import { argsToFieldConfigArgumentMap } from "graphql-tools";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation : {
+        toggleLike : async (_, args, { request }) => {
+            isAuthenticated(request);
+            const { postId } = args;
+            const { user } = request;
+            try {
+                const existingLike = await prisma.$exists.like({
+                    AND : [
+                        {
+                            user:{
+                                id: user.id
+                            }
+                        },
+                        {
+                            post: {
+                                id: postId
+                            }
+                        }
+                    ]
+                })
+                if (existingLike){
+                    // To Do (delete like)
+                } else {
+                    await prisma.createLike({
+                        user: {
+                            connect: {
+                                idZ: user.id
+                            }
+                        },
+                        post : {
+                            connect: {
+                                id: postId
+                            }
+                        }
+                    }) 
+                }
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
-
+import "./env";
 import logger from "morgan";
 import { GraphQLServer } from "graphql-yoga";
 import schema from "./schema";
-import { sendSecretMail } from "./utils";
+import passport from "passport";
+import "./passport";
+import { authenticateJwt } from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+    schema,
+    context: ({ request }) => ({ request }) // context를 사용하여 resolver 사이에서 정보 공유 : 다른 resolver에서 import 안해도 됨
+   }); 
 
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({ port : PORT }, () =>
     console.log(`Server running on http://localhost/${PORT}`)
